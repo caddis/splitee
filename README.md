9c5ee367bc0edb8f7dd5dc5c1f18acdbbd97acf3
Michael Leigeber <michael@caddis.co>
2013-09-26 21:55:40 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-splitee
-=======
+# ExpressionEngine Content Splitter
 
-ExpressionEngine plugin for splitting content into a set number of sections.
+This extensions splits content into a set number of sections.
+
+## Parameters
+
+```php
+splitter="\n" - next element to split content at (defaults to new line)
+columns="2" - number of columns to split content into
+delimiter="</div><div>" - seperator to be returned between split column paragraphs
+```
+
+## Usage
+
+```html
+<div>
+{exp:splitee delimiter="</div><div>"}
+<p>Test 1</p>
+<p>Test 2</p>
+{/exp:splitee}
+</div>
+
+<div>
+<p>Test 1</p>
+</div>
+<div>
+<p>Test 2</p>
+</div>
+```
+
+## License
+
+Copyright 2013 Caddis Interactive, LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
---
+++ system/expressionengine/third_party/splitee/pi.splitee.php
@@ -0,0 +1,85 @@
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+$plugin_info = array (
+	'pi_name' => 'SplitEE',
+	'pi_version' => '1.0.0',
+	'pi_author' => 'Michael Leigeber',
+	'pi_author_url' => 'http://www.caddis.co',
+	'pi_description' => 'Split a body of content into a set number of sections.',
+	'pi_usage' => Splitee::usage()
+);
+
+class Splitee {
+
+	public $return_data = '';
+
+	public function __construct()
+	{
+		// Set string to tagdata
+
+		$string = ee()->TMPL->tagdata;
+
+		// Fetch parameters
+
+		$splitter = ee()->TMPL->fetch_param('spltter', '\n');
+		$columns = ee()->TMPL->fetch_param('columns', 2);
+		$delimiter = ee()->TMPL->fetch_param('delimiter', '</div><div>');
+
+		// Split the content
+
+		$length = strlen($string);
+		$split = ceil($length / $columns);
+
+		$result = '';
+
+		if (preg_match('/^(.{' . $split . '}[^' . $splitter . ']*)(.+)$/s', $string, $matches))
+		{
+			array_shift($matches);
+
+			$i = 0;
+
+			foreach ($matches as $key => $value)
+			{
+				$result .= (($i > 0) ? $delimiter : '') . $value;
+
+				$i++;
+			}
+		}
+
+		$this->return_data = $result;
+	}
+
+	function usage()
+	{
+		ob_start();
+?>
+Parameters:
+
+splitter="\n" - next element to split content at (defaults to new line)
+columns="2" - number of columns to split content into
+delimiter="</div><div>" - seperator to be returned between split column paragraphs
+
+Usage:
+
+<div>
+{exp:splitee delimiter="</div><div>"}
+<p>Test 1</p>
+<p>Test 2</p>
+{/exp:splitee}
+</div>
+
+<div>
+<p>Test 1</p>
+</div>
+<div>
+<p>Test 2</p>
+</div>
+<?php
+		$buffer = ob_get_contents();
+
+		ob_end_clean();
+
+		return $buffer;
+	}
+}
+?>
